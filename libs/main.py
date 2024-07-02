bf41f0504953e710e7a87e08615a45a4e14eb6a7
AGcZNewtype <w1059493848@gmail.com>
2024-07-02 07:48:55 +0100

    fix bugs and file save function

--- libs/main.py
+++ libs/main.py
@@ -4,13 +4,17 @@ from pathlib import Path
 import base64
 import text_unit
 import GPT_api
+import os
 from BERT_model import BertModelWrapper
 
-#临时会话框
+
+
+#认证临时会话框
 @st.experimental_dialog("Please answer the question!",width="large")
-def authenticator(ques,ans):
+def authenticator(ques, ans, tmp_file, original_filename):
+
     st.write("请回答以下问题?")
-    # st.write(ques,ans)
+    # print(ques,ans)
     evidence_list = []
 
     #遍历问题和会话框
@@ -35,9 +39,15 @@ def authenticator(ques,ans):
                 evidence = evidence_list[i]
                 total_similarity.append(BertModel.sentence_match(evidence, ans[i]))
 
-            # text_unit.avg_similarity(total_similarity)
-            st.write(text_unit.avg_similarity(total_similarity))
+            similarity = text_unit.avg_similarity(total_similarity)
+            # print(text_unit.avg_similarity(total_similarity))
+
+            #保存并关闭临时文件
+            save_uploaded_file(tmp_file, original_filename)
+            os.remove(tmp_file)
             st.rerun()
+
+
         else:
             #清空对话框
             evidence_list = []
@@ -45,11 +55,29 @@ def authenticator(ques,ans):
 
 
 
+
+
+
+#保存文件方法
+def save_uploaded_file(tmp_file_path, original_filename):
+    # 定义保存路径
+    save_dir = Path("upload_files")
+    save_dir.mkdir(parents=True, exist_ok=True)
+
+    # 保存上传的文件
+    saved_file_path = save_dir / original_filename
+    with open(saved_file_path, "wb") as f:
+        f.write(Path(tmp_file_path).read_bytes())
+
+
 def upload():
     file = st.file_uploader("选择待上传的PDF文件", type=['pdf'])
 
+
     #预览上传文件
     if file is not None:
+        file_name = file.name
+
         with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
             fp = Path(tmp_file.name)
             fp.write_bytes(file.getvalue())
@@ -60,19 +88,22 @@ def upload():
             st.markdown(pdf_display, unsafe_allow_html=True)
 
             if st.button("验证作者身份",use_container_width=True):
-                # 读取PDF文件
+
+                #转化文件为纯文本
                 file = text_unit.extract_text_from_pdf(tmp_file.name)
 
                 # 使用GPT生成问题和答案
-                # response = GPT_api.GPT_generation(file)
+                response = GPT_api.GPT_generation(file)
 
                 # 对GPT回答进行切分归类处理
-                # ques, ans = text_unit.response_split(response)
-                ques = ["111111","222222"]
-                ans = ["1","2"]
+                ques, ans = text_unit.response_split(response)
+
 
                 #打开临时会话，回答问题并进行验证(同时传入文件以进行保存)
-                authenticator(ques,ans,file)
+                authenticator(ques, ans, tmp_file.name, file_name)
+
+
+
 
 
 
